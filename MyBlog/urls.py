37b19ecfd60247837620e8caaf38f0863d666152
Shreyas Patil <shreyaspatilg@gmail.com>
2020-04-28 16:47:08 +0530

    Made views from classes #2

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -1,15 +1,16 @@
-from django.urls import path
-from . import views
 from django.contrib.auth import views as auth_views
-from .views import signup_view
+from django.contrib.auth.decorators import login_required
+from django.urls import path
+
+from .views import AddPostView, SignUpView, IndexView, PostView, PostEditView, PostDeleteView
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('signup/', signup_view, name='signup'),
+    path('', IndexView.as_view(), name='index'),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('signin/', auth_views.LoginView.as_view(), name='signin'),
     path('signout/', auth_views.LogoutView.as_view(), name='signout'),
-    path('new/', views.addpost_view, name='new_post'),
-    path('post/<int:post_id>/', views.postview_view, name='post'),
-    path('post/<int:post_id>/edit/', views.postedit_view, name='editpost'),
-    path('post/<int:post_id>/delete/', views.postdelete_view, name='deletepost'),
+    path('new/', login_required(AddPostView.as_view(), login_url='/blog/signin/'), name='new_post'),
+    path('post/<int:post_id>/', PostView.as_view(), name='post'),
+    path('post/<int:post_id>/edit/', PostEditView.as_view(), name='editpost'),
+    path('post/<int:post_id>/delete/', PostDeleteView.as_view(), name='deletepost'),
 ]
--- MyBlog/views.py
+++ MyBlog/views.py
@@ -1,117 +1,156 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
-
-from .models import Post
 from .forms import SignUpForm, PostForm, SearchForm
+from .models import Post
 
 
-def index(request):
-    form = SearchForm(request.GET)
-    posts = Post.objects.filter(is_published=True)
+class IndexView(View):
+    form_class = SearchForm
 
-    drafts = None
-    if request.user is not None:
-        if request.user.is_authenticated:
-            drafts = Post.objects.filter(user=request.user).filter(is_published=False)
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(request.GET)
+        posts = Post.objects.filter(is_published=True)
 
-    if form.is_valid():
-        category = int(form.cleaned_data.get('category'))
-        tag = str(form.cleaned_data.get('tag')).strip().lower()
+        drafts = None
+        if request.user is not None:
+            if request.user.is_authenticated:
+                drafts = Post.objects.filter(user=request.user).filter(is_published=False)
 
-        if category != 0:
-            posts = posts.filter(category_id=category)
+        if form.is_valid():
+            category = int(form.cleaned_data.get('category'))
+            tag = str(form.cleaned_data.get('tag')).strip().lower()
 
-        if tag.isalnum():
-            posts = posts.filter(tags__contains=[tag])
+            if category != 0:
+                posts = posts.filter(category_id=category)
 
-        posts = posts.all()
+            if tag.isalnum():
+                posts = posts.filter(tags__contains=[tag])
 
-    else:
-        form = SearchForm()
-        posts = posts.all().order_by('-id')
+            posts = posts.all()
 
-    return render(request, 'home.html', {
-        'posts': posts,
-        'drafts': drafts,
-        'form': form
-    })
+        else:
+            form = SearchForm()
+            posts = posts.all().order_by('-id')
 
-
-def signup_view(request):
-    form = SignUpForm(request.POST)
-    if form.is_valid():
-        first_name = form.cleaned_data.get('first_name')
-        last_name = form.cleaned_data.get('last_name')
-        email = form.cleaned_data.get('email')
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-
-        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email,
-                                        password=password)
-
-        user.save()
-        return redirect('signin')
-    else:
-        form = SignUpForm()
-    return render(request, 'registration/signup.html', {'form': form})
+        return render(request, 'home.html', {
+            'posts': posts,
+            'drafts': drafts,
+            'form': form
+        })
 
 
-def addpost_view(request):
-    form = PostForm(request.POST)
+class SignUpView(View):
+    form_class = SignUpForm
 
-    if form.is_valid():
-        title = form.cleaned_data.get('title')
-        content = form.cleaned_data.get('content')
-        category = form.cleaned_data.get('category')
-        tags_data = str(form.cleaned_data.get('tags')).split(",")
-        tags = []
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, 'registration/signup.html', {'form': form})
 
-        for tag in tags_data:
-            tag_string = tag.strip().lower()
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
 
-            if len(tag_string) != 0:
-                tags.append(tag_string)
+        if form.is_valid():
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            email = form.cleaned_data.get('email')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
 
-        post = Post(
-            user=request.user,
-            title=title,
-            content=content,
-            tags=tags,
-            category_id=category
-        )
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email,
+                                            password=password)
 
-        if request.POST.get('action', '') == 'save_draft':
-            post.is_published = False
-
-        post.save()
-        return redirect('index')
-    else:
-        form = PostForm()
-
-    return render(request, 'add_post.html', {'form': form})
+            user.save()
+            return redirect('signin')
+        else:
+            form = SignUpForm()
+        return render(request, 'registration/signup.html', {'form': form})
 
 
-def postview_view(request, post_id):
-    try:
-        post = Post.objects.get(id=post_id)
-    except ObjectDoesNotExist:
-        post = None
+class AddPostView(View):
+    form_class = PostForm
 
-    return render(request, 'view_post.html', {'post': post})
+    def get(self, request):
+        form = self.form_class()
+        return render(request, 'add_post.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            content = form.cleaned_data.get('content')
+            category = form.cleaned_data.get('category')
+            tags_data = str(form.cleaned_data.get('tags')).split(" ")
+            tags = []
+
+            for tag in tags_data:
+                tag_string = tag.strip().lower()
+
+                if len(tag_string) != 0:
+                    tags.append(tag_string)
+
+            post = Post(
+                user=request.user,
+                title=title,
+                content=content,
+                tags=tags,
+                category_id=category
+            )
+
+            if request.POST.get('action', '') == 'save_draft':
+                post.is_published = False
+
+            post.save()
+            return redirect('index')
+        else:
+            form = PostForm()
+        return render(request, 'add_post.html', {'form': form})
 
 
-def postedit_view(request, post_id):
-    try:
+class PostView(View):
+    def get(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+        except ObjectDoesNotExist:
+            post = None
+        return render(request, 'view_post.html', {'post': post})
+
+
+class PostEditView(View):
+    def get(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+            tags = ""
+            for tag in post.tags:
+                tag_string = tag.strip().lower()
+
+                if len(tag_string) != 0:
+                    tags = tags + tag_string + " "
+
+            form = PostForm(initial={
+                'title': post.title,
+                'content': post.content,
+                'tags': tags,
+                'category': post.category
+            })
+
+        except ObjectDoesNotExist:
+            post = None
+            form = None
+
+        return render(request, 'edit_post.html', {'form': form, 'post': post})
+
+    def post(self, request, post_id):
         post = Post.objects.get(id=post_id)
         form = PostForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data.get('title')
             content = form.cleaned_data.get('content')
             category = form.cleaned_data.get('category')
-            tags_data = str(form.cleaned_data.get('tags')).split(",")
+            tags_data = str(form.cleaned_data.get('tags')).split(" ")
             tags = []
 
             for tag in tags_data:
@@ -132,34 +171,18 @@ def postedit_view(request, post_id):
 
             return redirect('index')
         else:
-            tags = ""
-            for tag in post.tags:
-                tag_string = tag.strip().lower()
-
-                if len(tag_string) != 0:
-                    tags = tags + tag_string + ","
-            form = PostForm(initial={
-                'title': post.title,
-                'content': post.content,
-                'tags': tags,
-                'category': post.category
-            })
-
-    except ObjectDoesNotExist:
-        form = None
-        post = None
-
-    return render(request, 'edit_post.html', {'form': form, 'post': post})
+            return render(request, 'edit_post.html', {'form': form, 'post': post})
 
 
-def postdelete_view(request, post_id):
-    try:
-        post = Post.objects.get(id=post_id)
+class PostDeleteView(View):
+    def get(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
 
-        if request.user == post.user:
-            post.delete()
+            if request.user == post.user:
+                post.delete()
 
-    except ObjectDoesNotExist:
-        post = None
+        except ObjectDoesNotExist:
+            post = None
 
-    return render(request, 'delete_post.html', {'post': post})
+        return render(request, 'delete_post.html', {'post': post})
